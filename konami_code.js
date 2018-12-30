303244b97f08deaefc360129b52e4c5aa07c2410
Vinit Rai <learner.vinit@gmail.com>
2018-12-30 04:20:49 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,22 @@ const codes = [
   "b",
   "a"
 ];
+let bodyContent = document.querySelector('body');
 
-function init() {
+function init(event) {
   // your code here
+  let index = 0;
+  const key = event.key;
+  if(key === codes[index]){
+    index++;
+    if(index === codes.length){
+      alert('congratulations to you');
+      index = 0;
+    }
+  }
+   else{
+     index = 0;
+   }
 }
+
+bodyContent.addEventListener('keydown',init);
